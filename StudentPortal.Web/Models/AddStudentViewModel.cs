77b1fdf6d536d3ac8397779242969ef7ce2cacb9
banda <banda@MSI>
2025-12-01 13:52:51 -0800

    Add project files.

---
+++ StudentPortal.Web/Models/AddStudentViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentPortal.Web.Models
+{
+    public class AddStudentViewModel
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Phone { get; set; }
+
+        public bool Subscribed { get; set; }
+    }
+}
